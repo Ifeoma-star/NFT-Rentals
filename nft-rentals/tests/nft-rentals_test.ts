4da013622a7f6963a9890f7e1b6f372033476679
Ifeoma-star <ujahnaomi104@gmail.com>
2025-01-31 13:17:09 +0100

    Add a test case to ensure that only contract owner can create rentals

--- nft-rentals/tests/nft-rentals_test.ts
+++ nft-rentals/tests/nft-rentals_test.ts
@@ -3,24 +3,31 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+    name: "Ensure that only contract owner can create rentals",
+    async fn(chain: Chain, accounts: Map<string, Account>)
+    {
+        const deployer = accounts.get("deployer")!;
+        const wallet1 = accounts.get("wallet_1")!;
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+        let block = chain.mineBlock([
+            // Try creating rental as non-owner
+            Tx.contractCall(
+                "nft-rentals",
+                "create-rental",
+                [types.uint(1), types.uint(100), types.uint(5000)],
+                wallet1.address
+            ),
+            // Create rental as owner
+            Tx.contractCall(
+                "nft-rentals",
+                "create-rental",
+                [types.uint(1), types.uint(100), types.uint(5000)],
+                deployer.address
+            )
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
+
+        assertEquals(block.receipts[0].result, `(err u100)`); // err-owner-only
+        assertEquals(block.receipts[1].result, `(ok u0)`);
+        assertEquals(block.height, 2);
+    }
 });
